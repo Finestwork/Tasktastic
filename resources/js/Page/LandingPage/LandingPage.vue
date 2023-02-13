b2470a86c94a8cd629dbc2d3bc9c302ef3a88781
Finestwork <Younes.Espiritu@outlook.com>
2023-02-14 04:01:21 +0800

    Separated login form components

--- resources/js/Page/LandingPage/LandingPage.vue
+++ resources/js/Page/LandingPage/LandingPage.vue
@@ -1,36 +1,21 @@
 <template>
     <div class="landing-page">
-        <form class="form">
-            <FormHeader />
-            <FormFields />
-            <p class="form__no-account">
-                Not registered yet? Create an account <a href="#">here</a>
-            </p>
-        </form>
+        <LoginForm />
     </div>
 </template>
 
 <script>
-import FormHeader from './Partials/FormHeader';
-import FormFields from './Partials/FormFields';
+import LoginForm from './Partials/LoginForm/LoginForm';
 
 export default {
     components: {
-        FormHeader,
-        FormFields,
+        LoginForm,
     },
 };
 </script>
 
 <style lang="scss">
-@use 'sass:map';
-@use '../../../scss/1-Settings/css-properties/font-size/major-second';
-@use '../../../scss/1-Settings/css-properties/colors/main';
-@use '../../../scss/1-Settings/css-properties/colors/text';
-@use '../../../scss/2-Tools/mixins/css-properties/width-and-height';
-@use '../../../scss/2-Tools/mixins/css-properties/font-size';
 @use '../../../scss/2-Tools/mixins/css-properties/padding';
-@use '../../../scss/2-Tools/mixins/css-properties/margin';
 
 // prettier-ignore
 .landing-page{
@@ -43,127 +28,5 @@ export default {
     @include padding.vertical((
         xsm: 10
     ));
-
-    .form{
-        width: 95%;
-        max-width: 350px;
-        background-color: white;
-        border-radius: 15px;
-        box-shadow: inset 0 -6px lighten(map.get(main.$primary, 200), 0);
-        @include padding.all-sides((
-            xsm: [25, 12, 20, 12]
-        ));
-
-        &__header{
-            .header{
-                &__logo{
-                    display: flex;
-                    @include width-and-height.set((
-                        xsm: (width: 25px)
-                    ));
-                    img{
-                        width: 100%;
-                    }
-                }
-                &__tagline{
-                    font-weight: 500;
-                    color: map.get(main.$primary, 900);
-                    @include font-size.responsive((
-                        xsm: map.get(major-second.$scale, 2)
-                    ));
-                    @include margin.top((
-                        xsm: 7
-                    ));
-                }
-            }
-        }
-        &__fields{
-            @include margin.top((
-                xsm: 15
-            ));
-
-            .text-input--faded--primary{
-                @include margin.top((
-                    xsm: 15
-                ));
-                &:last-of-type{
-                    margin-bottom: 0;
-                }
-            }
-
-            .btn--flat--primary{
-                display: flex;
-                justify-content: center;
-                width: 100%;
-                max-width: 150px;
-                margin-left: auto;
-                margin-right: auto;
-                transition: all .15s ease-in-out;
-                background-color: map.get(main.$primary, 900);
-                box-shadow: 0 6px rgba(map.get(main.$primary, 900), 0.4);
-                @include padding.vertical((
-                    xsm: 15
-                ));
-                @include margin.top((
-                    xsm: 25
-                ));
-                &:hover{
-                    background-color: lighten(map.get(main.$primary, 900), 5%);
-                }
-
-                &.btn--loading{
-                    background-color: rgba(map.get(main.$primary, 900), 0.3);
-                }
-
-                &:focus,
-                &.btn--loading{
-                    transform: translateY(4px);
-                    box-shadow: 0 2px rgba(map.get(main.$primary, 900), 0.4);
-                }
-            }
-
-            .forgot-pw-btn{
-                font-weight: 600;
-                background-color: white;
-                font-family: inherit;
-                border: none;
-                cursor: pointer;
-                color: map.get(main.$primary, 900);
-                @include font-size.responsive((
-                    xsm: map.get(major-second.$scale, 2)
-                ));
-                &:focus{
-                    outline: none;
-                }
-
-                &:focus,
-                &:hover{
-                    text-decoration: underline;
-                    color: lighten(map.get(main.$primary, 900), 15%);
-                }
-            }
-        }
-        &__no-account{
-            font-weight: 500;
-            color: map.get(text.$main, 400);
-            @include margin.top((
-                xsm: 45
-            ));
-            @include font-size.responsive((
-                xsm: map.get(major-second.$scale, 2)
-            ));
-
-            a{
-                color: map.get(main.$primary, 500);
-                &:focus{
-                    outline: none;
-                    text-decoration: underline;
-                }
-                &:hover{
-                    color: darken(map.get(main.$primary, 500), 15%);
-                }
-            }
-        }
-    }
 }
 </style>
---
+++ resources/js/Page/LandingPage/Partials/LoginForm/Partials/FormHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="form__header">
+        <span class="header__logo">
+            <img src="/assets/images/logos/logo.svg" alt="Tasktastic's Logo" />
+        </span>
+        <p class="header__tagline">
+            Spark your productivity with a tasty to-do list!
+        </p>
+    </div>
+</template>
